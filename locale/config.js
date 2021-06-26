a05afcb32a9853b75be2a619c0e9f15a0ea9607b
Dilip <dilip.cauti@gmail.com>
2021-06-26 17:28:31 +0530

    added hi
    hi added

--- locale/config.js
+++ locale/config.js
@@ -1,6 +1,6 @@
-// source: https://unpkg.com/dayjs@v1.10.4/locale/xx.js
-// see: https://github.com/iamkun/dayjs/blob/v1.10.4/src/locale/xx.js
+// source: https://unpkg.com/dayjs@v1.10.4/locale/hi.js
+// see: https://github.com/iamkun/dayjs/blob/v1.10.4/src/locale/hi.js
 
-// TODO: copy compiled translations from https://unpkg.com/dayjs@v1.10.4/locale/xx.js
+// TODO: copy compiled translations from https://unpkg.com/dayjs@v1.10.4/locale/hi.js
 
-dayjs.locale('xx');
+dayjs.locale('hi');
